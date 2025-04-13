b82f045b45e8f9a7819f5625ea5d04349b4d1e49
Nayem1004 <nayem@procoder.org>
2025-04-13 12:35:35 +0600

    even and odd number finding by array

---
+++ Array/even-odde.js
@@ -0,0 +1,8 @@
+var arr = [2,3,5,4,3,4,8,9,8,0,4,7]
+for (var i = 0; i <arr.length; i++){
+    if (arr[i]%2===0){
+        console.log('Even' + (arr[i]))
+    }else{
+        console.log('odd' + (arr[i]))
+    }
+}
